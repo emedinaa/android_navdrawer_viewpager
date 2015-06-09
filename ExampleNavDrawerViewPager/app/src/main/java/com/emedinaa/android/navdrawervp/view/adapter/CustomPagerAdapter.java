acd8f49655338de0fbddd4ff4ca6a6704d02b039
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-06-09 15:03:23 -0500

    ajustes view pager

---
+++ ExampleNavDrawerViewPager/app/src/main/java/com/emedinaa/android/navdrawervp/view/adapter/CustomPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.emedinaa.android.navdrawervp.view.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.emedinaa.android.navdrawervp.R;
+
+/**
+ * Created by emedinaa on 9/06/15.
+ */
+public class CustomPagerAdapter extends PagerAdapter {
+
+    Context mContext;
+    LayoutInflater mLayoutInflater;
+    private  int[] mResources = {
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher
+    };
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mResources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.row_pager, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(mResources[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
---
+++ ExampleNavDrawerViewPager/app/src/main/java/com/emedinaa/android/navdrawervp/view/listener/OnFragmentListener.java
@@ -0,0 +1,8 @@
+package com.emedinaa.android.navdrawervp.view.listener;
+
+/**
+ * Created by emedinaa on 31/03/15.
+ */
+public interface OnFragmentListener {
+    void gotoAction(Object obj);
+}
